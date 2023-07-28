d8cf35f9510c4ef7932e40300eb72f219ec5d436
Darrion Dumlao <darrion@Darrions-MBP.hsd1.ca.comcast.net>
2023-07-27 20:32:32 -0700

    added in cms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,109 +2,59 @@ import ProjectCard from '@/components/ProjectCard'
 import Row from '@/components/Row'
 import '@/styles/index.css'
 
-const rows = {
-	skills: [
+const getData = async () => {
+	const res = await fetch(
+		'https://api-us-west-2.hygraph.com/v2/clklz0k2z0wjq01t75jcnhnjc/master',
 		{
-			name: 'JavaScript',
-			imageAsset: '/images/download.jpeg',
-			description: 'blah blah',
-		},
-		{
-			name: 'JavaScript',
-			imageAsset: '/images/practice.png',
-			description: 'blah blah',
-		},
-		{
-			name: 'JavaScript',
-			imageAsset: '/images/practice.png',
-			description: 'blah blah',
-		},
-		{
-			name: 'JavaScript',
-			imageAsset: '/images/practice.png',
-			description: 'blah blah',
-		},
-		{
-			name: 'JavaScript',
-			imageAsset: '/images/download.jpeg',
-			description: 'blah blah',
-		},
-	],
-	projects: [
-		{
-			name: 'Reddit',
-			description: 'Rebuilt reddit using nextjs',
-			category: 'projects',
-			imageAsset: '/images/download.jpeg',
-		},
-		{
-			name: 'Discord',
-			description: 'Creating a discord bot',
-			category: 'projects',
-			imageAsset: '/images/download.jpeg',
-		},
-		{
-			name: 'music',
-			description: 'spotify clone',
-			category: 'rebuilds',
-			imageAsset: '/images/download.jpeg',
-		},
-	],
-	music: [
-		{
-			name: 'music',
-			description: 'spotify clone',
-			category: 'rebuilds',
-			imageAsset: '/images/download.jpeg',
-		},
-		{
-			name: 'music',
-			description: 'spotify clone',
-			category: 'rebuilds',
-			imageAsset: '/images/download.jpeg',
-		},
-		{
-			name: 'music',
-			description: 'spotify clone',
-			category: 'rebuilds',
-			imageAsset: '/images/download.jpeg',
-		},
-		{
-			name: 'music',
-			description: 'spotify clone',
-			category: 'rebuilds',
-			imageAsset: '/images/download.jpeg',
-		},
-		{
-			name: 'music',
-			description: 'spotify clone',
-			category: 'rebuilds',
-			imageAsset: '/images/download.jpeg',
-		},
-	],
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify({
+				query: `query Projects {
+          projects {
+            createdAt
+            description
+            id
+            name
+            publishedAt
+            updatedAt
+            category
+            url
+            image {
+              url
+            }
+          }
+        }`,
+			}),
+		}
+	)
+	const data = await res.json()
+	console.log(data)
+	return data.data.projects
 }
 
-const Home = () => {
+const Home = async () => {
+	const projects = await getData()
 	return (
 		<div className='px-12'>
 			<div className='place-content-center divide-y divide-solid divide-white/20'>
 				<div className='my-6'>
-					<Row row={rows.skills} title='Skills' />
+					<Row
+						row={projects.filter((project) => {
+							return project.category === 'Skills'
+						})}
+						title='Skills'
+					/>
 				</div>
 				<div className='my-6 pt-2.5'>
-					<Row row={rows.projects} title='Projects' />
-				</div>
-				<div className='my-6 pt-2.5'>
-					<Row row={rows.projects} title='Projects' />
-				</div>
-				<div className='my-6 pt-2.5'>
-					<Row row={rows.projects} title='Projects' />
-				</div>
-				<div className='my-6 pt-2.5'>
-					<Row row={rows.projects} title='Projects' />
-				</div>
-				<div className='my-6 pt-2.5'>
-					<Row row={rows.projects} title='Projects' />
+					<Row
+						row={projects.filter((project) => {
+							return project.category === 'Projects'
+						})}
+						title='Projects'
+					/>
 				</div>
 			</div>
 		</div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,15 +3,19 @@ import imageAsset from '/images/practice.png'
 
 const ProjectCard = ({ project }) => {
 	return (
-		<div className='group'>
-			<div
-				style={{ '--image-url': `url(${project.imageAsset})` }}
-				className='bg-[image:var(--image-url)] bg-no-repeat bg-cover w-full rounded-lg  aspect-square mb-2'>
-				<div className='w-full h-full bg-black/20 hidden group-hover:block'></div>
+		<a target='_blank' href={project.url}>
+			<div className='group'>
+				<div
+					style={{
+						'--image-url': `url(${project.imageAsset ?? project.image.url})`,
+					}}
+					className='bg-[image:var(--image-url)] bg-no-repeat bg-cover w-full rounded-lg  aspect-square mb-2'>
+					<div className='w-full h-full bg-black/20 hidden group-hover:block'></div>
+				</div>
+				<div className=' text-sm text-white/90'>{project.name}</div>
+				<div className='text-xs text-white/60'>{project.description}</div>
 			</div>
-			<div className=' text-sm text-white/90'>{project.name}</div>
-			<div className='text-xs text-white/60'>{project.description}</div>
-		</div>
+		</a>
 	)
 }
 
